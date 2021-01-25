1b871b00e8d83ed7de9b83988c2e02c99d381125
fdev-design <fdevpooja1999@gmail.com>
2021-01-25 19:03:11 +0530

    commt

---
+++ main.js
@@ -0,0 +1,15 @@
+//  function myFunction(){
+//      document.getElementById("video-link").display= block;
+//     alert ("This is an alert dialog box");  
+//  }
+
+// $(document).ready(function(){
+//     $('#videolink').magnificPopup({
+//         type:'inline',
+//         midclick:true
+//     })
+// })
+
+ 
+
+
